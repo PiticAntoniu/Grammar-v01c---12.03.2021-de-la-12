5af4dcca49e119828398efbba73e0bbb4eeecebb
antoniu.pitic <antoniu.pitic@gmail.com>
2021-03-12 12:55:26 +0200

    - new form to edit Grammar

---
+++ Grammar v01c/GrammarEditorForm.Designer.cs
@@ -0,0 +1,139 @@
+﻿namespace Grammar_v01c
+{
+    partial class GrammarEditorForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.startSymbolTextBox = new System.Windows.Forms.TextBox();
+            this.terminalsTextBox = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.nonterminalsTextBox = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.productionsDataGridView = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.productionsDataGridView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(76, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Simbol de start";
+            // 
+            // startSymbolTextBox
+            // 
+            this.startSymbolTextBox.Location = new System.Drawing.Point(96, 26);
+            this.startSymbolTextBox.Name = "startSymbolTextBox";
+            this.startSymbolTextBox.Size = new System.Drawing.Size(100, 20);
+            this.startSymbolTextBox.TabIndex = 1;
+            // 
+            // terminalsTextBox
+            // 
+            this.terminalsTextBox.Location = new System.Drawing.Point(95, 59);
+            this.terminalsTextBox.Name = "terminalsTextBox";
+            this.terminalsTextBox.Size = new System.Drawing.Size(100, 20);
+            this.terminalsTextBox.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 59);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Terminale";
+            // 
+            // nonterminalsTextBox
+            // 
+            this.nonterminalsTextBox.Location = new System.Drawing.Point(95, 95);
+            this.nonterminalsTextBox.Name = "nonterminalsTextBox";
+            this.nonterminalsTextBox.Size = new System.Drawing.Size(100, 20);
+            this.nonterminalsTextBox.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 95);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(63, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Neterminale";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(13, 134);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(48, 13);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Productii";
+            // 
+            // productionsDataGridView
+            // 
+            this.productionsDataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.productionsDataGridView.Location = new System.Drawing.Point(95, 134);
+            this.productionsDataGridView.Name = "productionsDataGridView";
+            this.productionsDataGridView.Size = new System.Drawing.Size(240, 150);
+            this.productionsDataGridView.TabIndex = 7;
+            // 
+            // GrammarEditorForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(391, 316);
+            this.Controls.Add(this.productionsDataGridView);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.nonterminalsTextBox);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.terminalsTextBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.startSymbolTextBox);
+            this.Controls.Add(this.label1);
+            this.Name = "GrammarEditorForm";
+            this.Text = "GrammarEditorForm";
+            ((System.ComponentModel.ISupportInitialize)(this.productionsDataGridView)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox startSymbolTextBox;
+        private System.Windows.Forms.TextBox terminalsTextBox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox nonterminalsTextBox;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.DataGridView productionsDataGridView;
+    }
+}
